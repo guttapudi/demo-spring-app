810ecae441cb5dddbe7e04c060e8aa3dc928ed5d
guttapudi <guttapudi.karthik@gmail.com>
2021-01-05 18:49:33 +0800

    Update TestController.java

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 	@GetMapping("/ping")
 	public String ping() {
-		return "pong" + ( System.getenv("HOSTNAME") != null ? System.getenv("HOSTNAME") : "" ).trim();
+		return "pong" + ( System.getenv("HOSTNAME") != null ? " " + System.getenv("HOSTNAME") : "" ).trim();
 	}
 }

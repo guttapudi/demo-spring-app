593659f8057ba42b10439607440af9bf2359e59b
guttapudi <guttapudi.karthik@gmail.com>
2021-01-05 12:04:18 +0800

    Update TestController.java

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 	@GetMapping("/ping")
 	public String ping() {
-		return "new pong";
+		return "new_pong";
 	}
 }
